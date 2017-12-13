1916bcfc131a0419c9492119a6e9c71b41872ab0
GardenerHan <1129982332@qq.com>
2017-12-13 21:45:17 +0800

    update ,delte ,evict

--- helloword/src/com/ifox/hgx/hibernate/test/Test2.java
+++ helloword/src/com/ifox/hgx/hibernate/test/Test2.java
@@ -1,6 +1,7 @@
 package com.ifox.hgx.hibernate.test;
 
 import com.ifox.hgx.hibernate.helloword.News;
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -80,10 +81,84 @@ public class Test2 {
         System.out.println(news.getClass().getName());
     }
 
+    /*
+
+     */
     @Test
     public void testLoad(){
         session.clear();
-        News news = session.load(News.class,4); ;
+        News news = session.load(News.class,4);
         System.out.println(news.getClass().getName());
+    }
+    /*
+        update:
+        1.若更新一个持久化对象，不需要调用update方法，应为调用Transaction的commit()方法是，会先执行session的flush方法
+        2.跟新一个游离的对象，需要显示的调用session的update方法，可以将一个有利的对象转变成一个持久化对象
+        注意:
+            1.无论更新的游离对象是否和数据表的记录一样，都会有update的sql 的语句
+                如何能让update方法不在盲目的发update的sql语句?
+                    在News.hbm.xml class节点 设置: select-before-update="true",但通常不需要设置该属性（需要多发一条select语句）
+            2.若数据表没有对应的记录，但还是调用了update方法，会抛出异常
+            3，当update方法关联了一个游离对象，弱国Session的缓存中已经有相同的ID的持久化对象，会抛出异常
+                NonUniqueObjectException:因为session缓存中，不能有两个OID相同的对象。
+     */
+    @Test
+    public  void testUpdate(){
+        News news = session.get(News.class,4);//在第一个session缓存里面
+
+        transaction.commit();
+        session.close();
+
+//        news.setId(1000);
+
+        //一个新的session
+        session = sessionFactory.openSession() ;
+        transaction = session.beginTransaction() ;
+
+//        news.setAuthor("Oracle");
+
+     //   News news1 = session.get(News.class,4);
+        session.update(news);
+    }
+    @Test
+    public  void testSaveOrUpdate(){
+        /*
+        有OID:update
+        临时对象:insert
+        1.若OID 不是null，但数据表还有对应的记录，会抛出一个异常
+        2.了解:OID值等于ID的unsaved-value属性值的对象，也被认为是一个游离对象
+         */
+        News news = new News("DD","dd",new Date(new java.util.Date().getTime())) ;
+        news.setId(10);
+//        news.setId(10000);//抛出异常
+        session.saveOrUpdate(news);
+    }
+    /*
+    delete:执行删除操作:OID与数据表一条记录对应，就会准备执行delete操作,若OID在数据库中没有对应的记录，抛出异常OptimisticLockException
+    可以通过 <property name="use_identifier_rollback">true</property> 将删除OID的对象OID为nuyll
+    变成这样:News{id=null, title='AA', author='aa', date=2017-12-13} id = null
+     */
+    @Test
+    public void testDelete(){
+//        News news = new News() ;
+//        news.setId(11);
+        News news = session.get(News.class ,5 ) ;
+        session.delete(news);//预备删除
+
+        System.out.println(news);
+    }
+
+    /*
+    evict():从Session缓存中吧指定的持久化对象移除
+     */
+    @Test
+    public void testEvict(){
+        News news = session.get(News.class,1) ;
+        News news1 = session.get(News.class,2) ;
+        news.setTitle("AA");
+        news.setTitle("BB");
+
+        session.evict(news);
+
     }
 }
